58eba2e1b91f5bf99f959bda1985f641fff16458
Ammarhussain50 <hussainammar205@gmail.com>
2025-08-26 03:00:18 +0500

    table tak kaam krlea

---
+++ Frontend/StockPlatform/src/Components/Incomestatement/Incomestatement.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Incomestatement = (props: Props) => {
+  return (
+    <div>Incomestatement</div>
+  )
+}
+
+export default Incomestatement
--- Frontend/StockPlatform/src/Routes/Routes.tsx
+++ Frontend/StockPlatform/src/Routes/Routes.tsx
@@ -3,6 +3,9 @@ import App from "../App";
 import HomePage from "../Pages/HomePage/HomePage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
 import SearchPage from "../Pages/SearchPage/SearchPage";
+import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
+import IncomeStatement from "../Components/Incomestatement/Incomestatement";
+import DesignGuide from "../Pages/DesignGuide/DesignGuide";
 
 export const router = createBrowserRouter([
   {
@@ -11,7 +14,15 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <HomePage /> },
       { path: "search", element: <SearchPage /> },
-      { path: "company/:ticker", element: <CompanyPage /> },
+      { path: "design-guide", element: <DesignGuide /> },
+      {
+        path: "company/:ticker",
+        element: <CompanyPage />,
+        children: [
+          { path: "company-profile", element: <CompanyProfile /> },
+          { path: "income-statement", element: <IncomeStatement /> },
+        ],
+      },
     ],
   },
 ]);
